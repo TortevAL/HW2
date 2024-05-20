8483e3c76274b407d4037caa41908de716e5e58b
TortevAL <165218904+TortevAL@users.noreply.github.com>
2024-05-20 17:08:30 +0300

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+time_for_one_task = (Number_of_hours_spent / number_of_completed_tasks)
+print('Курс:', Course_name, ', Выполненых задач: ', number_of_completed_tasks, ', Затрачено времени:', Number_of_hours_spent, ', Среднее время выполнения:', time_for_one_task)
